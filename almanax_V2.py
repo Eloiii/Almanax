9caea98b38826cfd94060f18b07466148fb693d0
Eloiii <eloi.charra@free.fr>
2023-02-27 21:16:35 +0100

    add image + little cleanup

--- almanax_V2.py
+++ almanax_V2.py
@@ -1,10 +1,9 @@
-import urllib.request
 import re
-import datetime
-import os
-
 # Pour la fenêtre
 from tkinter import *
+from urllib.request import urlopen
+
+from PIL import ImageTk
 
 
 def better_reading(content):
@@ -18,14 +17,14 @@ def better_reading(content):
 
 
 def get_ressource():
-    page = urllib.request.urlopen("https://www.krosmoz.com/fr/almanax")
+    page = urlopen("https://www.krosmoz.com/fr/almanax")
     content = re.findall('<p class="fleft">(.+?)</p>', str(page.read()))[0]
     content = better_reading(content)
     return content
 
 
 def get_bonus():
-    page = urllib.request.urlopen("https://www.krosmoz.com/fr/almanax")
+    page = urlopen("https://www.krosmoz.com/fr/almanax")
     content = re.findall('<div class="more">(.+?)</div>', str(page.read()))[0]
     content = content.replace("<b>", "")
     content = content.replace("</b>", "")
@@ -36,11 +35,24 @@ def get_bonus():
 
 # On veut créer une nouvelle fonction qui récupère l'image de la ressource de l'almanax et la stocke dans une variable pour pouvoir l'afficher dans la fenêtre plus tard
 
+"""
+on récupère l'url de l'image qui est dans la balise <img ... /> dans itemImageSrc
+
+on obtient les octets de l'image avec le read
+
+on crée l'objet PhotoImage de Tkinter avec les données
+
+"""
 def get_image():
-    page = urllib.request.urlopen("https://www.krosmoz.com/fr/almanax")
-    # L'image se trouve dans la balise <img src="..."> dans la balise <div class="more-infos-content">
-    # On veut donc récupérer le nom de l'image en .png pour la stocker dans la variable img
-    img = re.findall('<div class="more-infos-content">.+<img src="(.+?)">', str(page.read()))
+    page = urlopen("https://www.krosmoz.com/fr/almanax")
+
+    itemImageTag = re.findall('<div class=\"more-infos-content\">(.+?)<img([\w\W]+?)/>', str(page.read()))[0][1]
+    itemImageSrc = re.findall('src=\"(.*?)\"', itemImageTag)[0]
+
+    image_byt = urlopen(itemImageSrc)
+    raw_data = image_byt.read()
+    image = ImageTk.PhotoImage(data=raw_data)
+    return image
 
 
 # Création de la fenêtre dans laquelle on affiche le resultat de la fonction get_ressource() et get_bonus()
@@ -61,6 +73,12 @@ label_ressource.pack()
 
 label_bonus = Label(window, text=get_bonus(), font=("Courrier", 20), bg='#41B77F', fg='white')
 label_bonus.pack()
+
+img = get_image()
+
+label_img = Label(image=img)
+label_img.image = img
+label_img.pack()
 
 # Affichage de l'image
 
