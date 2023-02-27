53489fb7f16af94b848976f6afa86164b7377ba1
Eloiii <eloi.charra@free.fr>
2023-02-27 20:34:35 +0100

    removing [0] (moche), bonne soirée bébou

--- almanax_V2.py
+++ almanax_V2.py
@@ -3,31 +3,36 @@ import re
 import datetime
 import os
 
-#Pour la fenêtre
+# Pour la fenêtre
 from tkinter import *
 
+
 def better_reading(content):
-    content[0] = content[0].replace("\\xc3\\xa9", "é")
-    content[0] = content[0].replace("\\xc3\\xa0", "à")
-    content[0] = content[0].replace("\\xc3\\xa8", "è")
-    content[0] = content[0].replace("\\xc3\\xa7", "ç")
-    content[0] = content[0].replace("\\'", "'")
-    content[0] = content[0].replace("\\n", "")
+    content = content.replace("\\xc3\\xa9", "é")
+    content = content.replace("\\xc3\\xa0", "à")
+    content = content.replace("\\xc3\\xa8", "è")
+    content = content.replace("\\xc3\\xa7", "ç")
+    content = content.replace("\\'", "'")
+    content = content.replace("\\n", "")
+    return content
+
 
 def get_ressource():
     page = urllib.request.urlopen("https://www.krosmoz.com/fr/almanax")
-    content = re.findall('<p class="fleft">(.+?)</p>', str(page.read()))
-    better_reading(content)
-    return content[0]
+    content = re.findall('<p class="fleft">(.+?)</p>', str(page.read()))[0]
+    content = better_reading(content)
+    return content
+
 
 def get_bonus():
     page = urllib.request.urlopen("https://www.krosmoz.com/fr/almanax")
-    content = re.findall('<div class="more">(.+?)</div>', str(page.read()))
-    content[0] = content[0].replace("<b>", "")
-    content[0] = content[0].replace("</b>", "")
-    content[0] = content[0].split(".")[0]
-    better_reading(content)
-    return content[0]
+    content = re.findall('<div class="more">(.+?)</div>', str(page.read()))[0]
+    content = content.replace("<b>", "")
+    content = content.replace("</b>", "")
+    content = content.split(".")[0]
+    content = better_reading(content)
+    return content
+
 
 # On veut créer une nouvelle fonction qui récupère l'image de la ressource de l'almanax et la stocke dans une variable pour pouvoir l'afficher dans la fenêtre plus tard
 
@@ -36,7 +41,8 @@ def get_image():
     # L'image se trouve dans la balise <img src="..."> dans la balise <div class="more-infos-content">
     # On veut donc récupérer le nom de l'image en .png pour la stocker dans la variable img
     img = re.findall('<div class="more-infos-content">.+<img src="(.+?)">', str(page.read()))
-    
+
+
 # Création de la fenêtre dans laquelle on affiche le resultat de la fonction get_ressource() et get_bonus()
 
 window = Tk()
